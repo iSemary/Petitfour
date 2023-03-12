df3c9c916e74965b7b436a55366b2f6fbc3205ca
Abdelrahman Samir <abdelrahman@notchnco.com>
2023-03-12 11:27:56 +0200

    Added contact us form

--- resources/js/src/Pages/Connect.js
+++ resources/js/src/Pages/Connect.js
@@ -1,46 +1,97 @@
 import { useState } from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+import Container from "react-bootstrap/esm/Container";
+import { FiSend } from "react-icons/fi";
 
 function Connect() {
     const [submit, setSubmit] = useState(false);
     const [formValues, setFormValues] = useState([]);
+
+    const Subjects = [
+        {
+            id: 1,
+            name: "Project",
+        },
+        {
+            id: 2,
+            name: "Vacancy Job",
+        },
+        {
+            id: 3,
+            name: "Per Hour",
+        },
+    ];
 
     const handleSubmit = (e) => {
         alert(JSON.stringify(formValues, null, 2));
         setSubmit(false);
     };
     return (
-        <div className="main-content">
-            <h1>Any place in your app!</h1>
-            <Formik
-                initialValues={{ email: "", password: "" }}
-                validate={(formValues) => {
-                    const errors = {};
-                    if (!formValues.email) {
-                        errors.email = "Required";
-                    } else if (
-                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
-                            formValues.email
-                        )
-                    ) {
-                        errors.email = "Invalid email address";
-                    }
-                    return errors;
-                }}
-                onSubmit={handleSubmit}
-            >
-                {({ submit }) => (
-                    <Form>
-                        <Field type="email" name="email" />
-                        <ErrorMessage name="email" component="div" />
-                        <ErrorMessage name="password" component="div" />
-                        <button type="submit" disabled={submit}>
-                            Submit
-                        </button>
-                    </Form>
-                )}
-            </Formik>
-        </div>
+        <Container>
+            <div className="main-content">
+                <Row>
+                    <Col md={6}>
+                        <h1>Any place in your app!</h1>
+                        <Form>
+                            <Form.Group
+                                className="mb-3"
+                                controlId="formBasicEmail"
+                            >
+                                <Form.Label>Full Name</Form.Label>
+                                <Form.Control
+                                    type="email"
+                                    placeholder="Enter email"
+                                />
+                            </Form.Group>
+                            <Form.Group
+                                className="mb-3"
+                                controlId="formBasicEmail"
+                            >
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control
+                                    type="email"
+                                    placeholder="Enter full name"
+                                />
+                            </Form.Group>
+                            <Form.Group
+                                className="mb-3"
+                                controlId="formBasicEmail"
+                            >
+                                <Form.Label>Subject</Form.Label>
+                                <Form.Select aria-label="Default select example">
+                                    <option value="">
+                                        Select your subject
+                                    </option>
+                                    {Subjects?.map((Subject, i) => (
+                                        <option value={Subject.id} key={i}>
+                                            {Subject.name}
+                                        </option>
+                                    ))}
+                                </Form.Select>
+                            </Form.Group>
+                            <Form.Group
+                                className="mb-3"
+                                controlId="formBasicSubject"
+                            >
+                                <Form.Label>Subject</Form.Label>
+                                <Form.Control
+                                    as="textarea"
+                                    name="message"
+                                    placeholder="Leave a comment here"
+                                />
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                <FiSend /> Submit
+                            </Button>
+                        </Form>
+                    </Col>
+                    <Col md={6}>Image</Col>
+                </Row>
+            </div>
+        </Container>
     );
 }
 
